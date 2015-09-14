6c9621ab036461c0079f8e74d13c142347663eb8
Jennifer Madrid <Mommy@jennifer-madrids-macbook.local>
2015-09-14 18:09:08 -0500

    Initial Commit

---
+++ PlantSwap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PlantSwap
+//
+//  Created by Jennifer Madrid on 9/14/15.
+//  Copyright © 2015 Jennifer Madrid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
